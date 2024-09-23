493372475fabb576d0b958cc6b3b5a35fd7c40ac
ImanArs <imanbaevemaill.ru@gmail.com>
2024-09-24 00:33:08 +0600

    loading screen

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ window.addEventListener('load', () => {
     loadingScreen.style.opacity = '0';
     setTimeout(() => {
       loadingScreen.style.display = 'none';
+      window.scrollTo(0, 0);
     }, 1000); // Убедитесь, что это значение совпадает с длительностью перехода в CSS
   }, 3000); // 5 секунд
 });
